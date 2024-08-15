d0a505cb350255f65415b5646894d7b5fd5e9c30
PROGRAMAÇÃO EM PYTHON - 2024.1 <47129532024.1@senacrjedu.local>
2024-08-14 21:28:10 -0300

    Atualização controle para incluir Html

--- python/controle/controle_visitantes/settings.py
+++ python/controle/controle_visitantes/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'controle_visitantes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- python/controle/visitantes/models.py
+++ python/controle/visitantes/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 class Visitante(models.Model):
-    usuario = models.OneToOneField(
-        "usuarios.Usuario",
-        verbose_name = "Usuário",
-        on_delete = models.PROTECT
-    )
-
     nome_completo = models.CharField(
         verbose_name = "Nome Completo",
-        max_length = 100,
+        max_length = 150,
     )
 
     cpf = models.CharField(
@@ -38,20 +32,20 @@ class Visitante(models.Model):
         max_length = 11,
     )
 
-    hora_da_chegada = models.TimeField(
-        verbose_name = "Hora da Chegada",
-        max_length = 11,
-        auto_now_add=False,
+    hora_da_chegada = models.DateTimeField(
+        verbose_name = "Hora de chegada na portaria",
+        auto_now_add=True,
     )
     
-    hora_da_saida = models.TimeField(
-        verbose_name = "Hora da Saída",
-        max_length = 11,
+    hora_da_saida = models.DateTimeField(
+        verbose_name = "Hora de saída do condomínio",
         auto_now_add=False,
+        blank=True,
+        null=True
     )
 
-    hora_da_autorizacao = models.TimeField(
-        verbose_name = "Hora da Autorização",
+    hora_da_autorizacao = models.DateTimeField(
+        verbose_name = "Hora de autorização de entrada",
         max_length = 11,
         auto_now_add=False,
     )
@@ -66,9 +60,16 @@ class Visitante(models.Model):
         max_length = 100,
     )
 
+    registrado_por = models.ForeignKey(
+        "porteiros.Porteiro",
+        verbose_name="Porteiro responsável pelo registro",
+        on_delete=models.PROTECT,
+    )
+
     class Meta:
-        verbose_name = "Visitante"
-        verbose_name_plural = "Visitantes"
-        db_table = "visitante"
+        verbose_name="Visitante"
+        verbose_name_plural="Visitantes"
+        db_table="visitante"
 
-
+    def __str__(self):
+        return self.nome_completo
