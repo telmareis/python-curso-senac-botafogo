b2ee6e8c38c29f8ff859ad6b0839659e3d71d351
PROGRAMAÇÃO EM PYTHON - 2024.1 <47129532024.1@senacrjedu.local>
2024-08-21 21:33:15 -0300

    url

--- python/controle/visitantes/views.py
+++ python/controle/visitantes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 def registrar_visitante(request):
  context = {}
- return render(request, "pagina.html", context)
+ return render(request, "registrar_visitante.html", context)
+
