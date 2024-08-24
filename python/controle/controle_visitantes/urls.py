9af31aebca7fd734931eb592830899dd5ddec64c
PROGRAMAÇÃO EM PYTHON - 2024.1 <47129532024.1@senacrjedu.local>
2024-08-23 21:36:23 -0300

    Form

--- python/controle/controle_visitantes/urls.py
+++ python/controle/controle_visitantes/urls.py
@@ -6,5 +6,5 @@ from visitantes.views import registrar_visitante
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="usuarios"),
-    path('registrar-visitante/', registrar_visitante, name="registrar_visitante"),
+    path('registrar_visitante/', registrar_visitante, name="registrar_visitante"),
 ]
